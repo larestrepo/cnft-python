ad32d3e89f3347efd9b44922de5506ba95dd5461
larestrepo <larestrepomu@gmail.com>
2021-09-24 09:21:16 -0400

    Pinata pin_file_to_ipfs working

--- main.py
+++ main.py
@@ -6,32 +6,10 @@ from pinata import PinataPy
 PINATA_API_KEY= config('PINATA_API_KEY')
 PINATA_SECRET_API_KEY = config('PINATA_SECRET_API_KEY')
 API_KEY = config('API_KEY')
-#path = ('./images/ARSSER') # place of the images to be uploaded
-
-# def file_reading(path):
-#     """ Generates dict with the list of images to be uploaded into IPFS """
-
-#     dirs = sorted([f for f in os.listdir(path) if not f.startswith('.')])
-#     img = {}
-#     for i in dirs:
-#         sub_dirs = os.path.join(path, i)
-#         files = sorted([sub_dir for sub_dir in os.listdir(sub_dirs) if not sub_dir.startswith('.')])
-#         img[i] = [s for s in files]
-#     return (img)
-
-#all_images = file_reading(path)
-#print(all_images)
-
-# image_list = []
-# for k, v in all_images.items():
-#     abs_path = path + '/' + k
-#     for i in v:
-#         image_path = abs_path + '/' + i
-#         image_list.append(image_path)
-# print(image_list)
+path = ('/images/ARSSER/Serie2') # place of the images to be uploaded
 
 dirname = os.path.dirname(__file__)
-path = dirname + '/images/ARSSER/Serie2'
+path = dirname + path
 c = PinataPy(PINATA_API_KEY,PINATA_SECRET_API_KEY)
 cid = c.pin_file_to_ipfs(path)
 print(cid)
--- pinata.py
+++ pinata.py
@@ -7,13 +7,23 @@ import requests
 API_ENDPOINT = "https://api.pinata.cloud/"
 
 class PinataPy:
+
+    """ This library is mainly used to upload files to pinata. There are more functionalities
+        available in the Pinata API but the most important is the function called pin_file_to_ipfs.
+        For future work: pin_hash_to_ipfs, pin_json_to_ipfs and pin_list among others. It is also
+        possible to provide metadata, but not yet implemented. 
+    """
     def __init__(self, pinata_api_key, pinata_secret_api_key):
-            self._auth_headers: Headers = {
+            self._auth_headers = {
                 "pinata_api_key": pinata_api_key,
                 "pinata_secret_api_key": pinata_secret_api_key,
             }
 
-    #@staticmethod
+    @staticmethod
+    def error(response):
+        """Construct dict from response if an error has occurred"""
+        return {"status": response.status_code, "reason": response.reason, "text": response.text}
+
     def pin_file_to_ipfs(self, path_to_file):
         """
         Pin any file, or directory, to Pinata's IPFS nodes
@@ -21,97 +31,35 @@ class PinataPy:
         More: https://docs.pinata.cloud/api-pinning/pin-file
         """
         url = API_ENDPOINT + "pinning/pinFileToIPFS"
-        headers: Headers = self._auth_headers
+        headers = self._auth_headers
 
         def get_all_files(directory):
             """get a list of absolute paths to every file located in the directory"""
             paths = []
-            for root, dirs, files_ in os.walk(os.path.abspath(directory)):
-                for file in files_:
+            for root, dirs, files in os.walk(os.path.abspath(directory)):
+                for file in files:
                     paths.append(os.path.join(root, file))
             return paths
 
-        #files: tp.Dict[str, tp.Any]
-        # pinataOptions= {'pinataOptions':{
-            
-        #                         'customPinPolicy': {
-        #                             'regions': [
-        #                                 {
-        #                                     'id': 'FRA1',
-        #                                     'desiredReplicationCount': 1
-        #                                 },
-        #                                 {
-        #                                     'id': 'NYC1',
-        #                                     'desiredReplicationCount': 2
-        #                                 }
-        #                             ]
-        #                         }
-        #                     }
-        # }
-        #options = {}
-        options = {'pinataOptions':{'cidVersion':0}}
-        
-                
-        print(json.dumps(options))        
+        options = {
+            'pinataOptions':{
+                'cidVersion':0,
+                'wrapWithDirectory': 'false',
+                'customPinPolicy': {
+                    'regions': [
+                        {
+                            'id': 'FRA1',
+                            'desiredReplicationCount': 1
+                        },
+                        {
+                            'id': 'NYC1',
+                            'desiredReplicationCount': 2
+                        }
+                    ]
+                }
+            }
+        }     
         all_files = get_all_files(path_to_file)       
-        data = [('file', (file, open(file, "rb"))) for file in all_files]
-        #data = [('file',(file,open(all_files[0],'rb')))]
-        #data = [('file',(all_files[0],open(all_files[0],'rb')))]
-        #headers['pinataOptions'] = options['pinataOptions']
-
-        #data.append(json.dumps(options))
-        print(data)
-        # if options is not None:
-        #     if "pinataMetadata" in options:
-        #         headers["pinataMetadata"] = options["pinataMetadata"]
-        #     if "pinataOptions" in options:
-        #         headers["pinataOptions"] = options["pinataOptions"]
-        response = requests.post(url=url, files = data, headers = headers )
-        #response = requests.post(self.url, files=files, headers=headers)
-        return response.json() if response.ok else self._error(response)  # type: ignore
-
-
-
-
-
-
-
-
-class NftStorage:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        # self.pinata_api_key = pinata_api_key
-        # self.pinata_secret_api_key = pinata_secret_api_key
-        self.url = 'https://api.pinata.cloud/pinning/pinFileToIPFS'
-        # self.headers = {
-        #     'pinata_api_key': pinata_api_key,
-        #     'pinata_secret_api_key': pinata_secret_api_key
-
-        # }
-        #self.url = 'https://api.nft.storage/upload'
-        self.headers = {'Authorization': 'Bearer ' + self.api_key}
-        
-    
-    def upload(self, file_list, file_type):
-        files = []
-        for i in file_list:
-            data = open(i, 'rb').read()
-            #files.append(('file', (i.split('/')[2], open(i, 'rb').read(), file_type)))
-            response = requests.post(self.url, headers = self.headers, data = data)
-            if response.json()['ok'] == True:
-                return response.json()['value']['cid']
-        # try:
-        #     response = requests.post(self.url, headers = self.headers, files = files)
-        #     if response.json()['ok'] == True:
-        #         return response.json()['value']['cid']
-        # except:
-        #     print("Something went wrong with the upload")
-            
-    # def upload(self, file):
-    #     data = open(file, 'rb').read()
-    #     try:
-    #         response = requests.post(self.url, headers = self.headers, data = data)
-    #         if response.json()['ok'] == True:
-    #             return response.json()['value']['cid']
-    #     except:
-    #         print("Something went wrong with the upload")
+        data = [('file', (open(file, "rb"))) for file in all_files]
+        response = requests.post(url=url, files = data, headers = headers, json = options )
+        return response.json() if response.ok else self.error(response)
